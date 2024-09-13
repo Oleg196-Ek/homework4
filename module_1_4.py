38cc2ac008d9777ca5e79c0bc5e19bfb662330f2
Oleg196-Ek <zhookov@mail.ru>
2024-09-13 14:35:34 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,12 @@
+# my_string = input("Text: ")
+# print(type("Text: "))
+# current_quantity = 7
+# how_much = int(input("сколько символов? "))
+# total = current_quantity - how_much
+# print(total)
+print('my_string'.upper())
+print('my_string'.lower())
+print('my _ string'.replace(' ', ''))
+txt = 'my_string'
+print(txt[0])
+print(txt[-1])
